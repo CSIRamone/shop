247124466ace909e30bc6450c9162e441a639ff1
CSIRamone <cidinei_inacio@hotmail.com>
2023-07-25 00:26:41 -0300

    Extraindo grid produtos

--- lib/models/pages/products_overview_page.dart
+++ lib/models/pages/products_overview_page.dart
@@ -1,34 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:shop/components/product_item.dart';
-import 'package:shop/models/product.dart';
-import 'package:provider/provider.dart';
 
-import '../product_list.dart';
+import '../../components/product_grid.dart';
 
 class ProductsOverviewPage extends StatelessWidget {
   const ProductsOverviewPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final provider = Provider.of<ProductList>(context);
-    final List<Product> loadedProducts = provider.items;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).primaryColor,
         title: const Text('Minha Loja'),
       ),
-      body: GridView.builder(
-        padding: const EdgeInsets.all(10),
-        itemCount: loadedProducts.length,
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-        ),
-        itemBuilder: ((context, index) =>
-            ProductItem(product: loadedProducts[index])),
-      ),
+      body: const ProductGrid(),
     );
   }
 }
