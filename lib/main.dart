7f5e0a6ca767ee2660ff61f0d29f981394de620d
CSIRamone <cidinei_inacio@hotmail.com>
2023-07-24 22:15:07 -0300

    exemplo Provider utilizando Inherited

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:shop/models/pages/counter_page.dart';
 import 'package:shop/models/pages/product_detail_page.dart';
 import 'package:shop/models/pages/products_overview_page.dart';
 import 'package:shop/models/product.dart';
+import 'package:shop/providers/counter.dart';
 import 'package:shop/utils/app_routes.dart';
 
 void main() {
@@ -14,21 +16,24 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        //colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        //useMaterial3: true,
-        primaryColor: Colors.purple.shade900,
-        hintColor: Colors.amber.shade900,
-        highlightColor: Colors.greenAccent,
-        fontFamily: 'Lato',
+    return CounterProvider(
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          //colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          //useMaterial3: true,
+          primaryColor: Colors.purple.shade900,
+          hintColor: Colors.amber.shade900,
+          highlightColor: Colors.greenAccent,
+          fontFamily: 'Lato',
+        ),
+        home: ProductsOverviewPage(),
+        routes: {
+          AppRoutes.PRODUCT_DETAIL: (context) =>
+              const CounterPage(), //ProductDetailPage(),
+        },
+        debugShowCheckedModeBanner: false,
       ),
-      home: ProductsOverviewPage(),
-      routes: {
-        AppRoutes.PRODUCT_DETAIL: (context) => const ProductDetailPage(),
-      },
-      debugShowCheckedModeBanner: false,
     );
   }
 }
