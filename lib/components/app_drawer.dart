4d7ce6d4967d50b941b36453dd4ce4327809b322
CSIRamone <cidinei_inacio@hotmail.com>
2023-08-08 18:35:20 -0300

    Componente Pedido 2

--- lib/components/app_drawer.dart
+++ lib/components/app_drawer.dart
@@ -11,6 +11,7 @@ class AppDrawer extends StatelessWidget {
         children: [
           AppBar(
             title: const Text('Bem vindo usuario!'),
+            automaticallyImplyLeading: false,
           ),
           const Divider(),
           ListTile(
--- lib/models/pages/products_overview_page.dart
+++ lib/models/pages/products_overview_page.dart
@@ -65,7 +65,7 @@ class _ProductsOverviewPageState extends State<ProductsOverviewPage> {
         ],
       ),
       body: ProductGrid(_showFavoriteOnly),
-      drawer: AppDrawer(),
+      drawer: const AppDrawer(),
     );
   }
 }
