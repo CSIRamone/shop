a157f436191f82d3ce82e197a81360ae17989d79
CSIRamone <cidinei_inacio@hotmail.com>
2023-08-07 23:11:11 -0300

    Detalhe produto page

--- lib/models/pages/product_detail_page.dart
+++ lib/models/pages/product_detail_page.dart
@@ -9,12 +9,45 @@ class ProductDetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final Product product =
-        ModalRoute.of(context)?.settings.arguments as Product;
+        ModalRoute.of(context)!.settings.arguments as Product;
     return Scaffold(
-        appBar: AppBar(
-      title: Text(
-        product.name,
+      appBar: AppBar(
+        title: Text(
+          product.name,
+        ),
       ),
-    ));
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.fromLTRB(2, 30, 2, 30),
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                product.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'R\$ ${product.price}',
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              width: double.infinity,
+              child: Text(
+                product.description,
+                textAlign: TextAlign.center,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
