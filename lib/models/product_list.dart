de86d9024733d9790bdd1c0cb46431173b36e404
CSIRamone <cidinei_inacio@hotmail.com>
2023-08-11 23:54:32 -0300

    delete product

--- lib/models/product_list.dart
+++ lib/models/product_list.dart
@@ -43,6 +43,15 @@ class ProductList with ChangeNotifier {
     }
   }
 
+  void deleteProduct(Product product) {
+    int index = _items.indexWhere((element) => element.id == product.id);
+
+    if (index >= 0) {
+      _items.removeWhere((element) => element.id == product.id);
+      notifyListeners();
+    }
+  }
+
   void addProduct(Product product) {
     _items.add(product);
     notifyListeners();
