b6eb47bd9e23614a8866b8342703a0f28bea85a6
CSIRamone <cidinei_inacio@hotmail.com>
2023-08-21 22:18:31 -0300

    Carregando Pedidos e progress indicator

--- lib/models/order_list.dart
+++ lib/models/order_list.dart
@@ -1,9 +1,12 @@
+import 'dart:async';
 import 'dart:convert';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
 
 import 'package:http/http.dart' as http;
+import 'package:provider/provider.dart';
+import 'package:shop/models/cart_item.dart';
 
 import '../utils/constants.dart';
 import 'cartNew.dart';
@@ -18,6 +21,34 @@ class OrderList with ChangeNotifier {
 
   int get itemsCount {
     return _items.length;
+  }
+
+  Future<void> loadOrders() async {
+    _items.clear();
+    final response =
+        await http.get(Uri.parse('${Constants.ORDER_BASE_URL}.json'));
+    if (response.body == 'null') return;
+    Map<String, dynamic> data = jsonDecode(response.body);
+    data.forEach((orderId, orderData) {
+      _items.add(
+        Order(
+          id: orderId,
+          date: DateTime.parse(orderData['date']),
+          total: orderData['total'],
+          products: (orderData['products'] as List<dynamic>).map((item) {
+            return CartItem(
+              id: item['id'],
+              productId: item['productId'],
+              name: item['name'],
+              quantity: item['quantity'],
+              price: item['price'],
+            );
+          }).toList(),
+        ),
+      );
+    });
+    notifyListeners();
+    //print(jsonDecode(response.body));
   }
 
   Future<void> addOrder(Cart cart) async {
--- lib/models/pages/order_page.dart
+++ lib/models/pages/order_page.dart
@@ -4,8 +4,36 @@ import 'package:shop/components/app_drawer.dart';
 import 'package:shop/components/order_widget.dart';
 import 'package:shop/models/order_list.dart';
 
-class OrdersPage extends StatelessWidget {
+class OrdersPage extends StatefulWidget {
   const OrdersPage({super.key});
+
+  @override
+  State<OrdersPage> createState() => _OrdersPageState();
+}
+
+class _OrdersPageState extends State<OrdersPage> {
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Provider.of<OrderList>(
+      context,
+      listen: false,
+    ).loadOrders().then((_) {
+      setState(() {
+        _isLoading = false;
+      });
+    });
+  }
+
+  Future<void> _refreshOrders(BuildContext context) {
+    return Provider.of<OrderList>(
+      context,
+      listen: false,
+    ).loadOrders();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,10 +44,18 @@ class OrdersPage extends StatelessWidget {
         title: const Text('Meus Pedidos'),
       ),
       drawer: const AppDrawer(),
-      body: ListView.builder(
-        itemCount: orders.itemsCount,
-        itemBuilder: (context, index) =>
-            OrderWidget(order: orders.items[index]),
+      body: RefreshIndicator(
+        onRefresh: () => _refreshOrders(context),
+        child: Padding(
+          padding: EdgeInsets.all(8),
+          child: _isLoading
+              ? const Center(child: CircularProgressIndicator())
+              : ListView.builder(
+                  itemCount: orders.itemsCount,
+                  itemBuilder: (context, index) =>
+                      OrderWidget(order: orders.items[index]),
+                ),
+        ),
       ),
     );
   }
