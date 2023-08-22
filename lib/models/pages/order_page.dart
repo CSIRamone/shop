03337541898de209dbdae55473d40f7eaad39eb6
CSIRamone <cidinei_inacio@hotmail.com>
2023-08-21 23:08:17 -0300

    Trabalhando com FutureBuilder

--- lib/models/pages/order_page.dart
+++ lib/models/pages/order_page.dart
@@ -4,58 +4,36 @@ import 'package:shop/components/app_drawer.dart';
 import 'package:shop/components/order_widget.dart';
 import 'package:shop/models/order_list.dart';
 
-class OrdersPage extends StatefulWidget {
+class OrdersPage extends StatelessWidget {
   const OrdersPage({super.key});
 
   @override
-  State<OrdersPage> createState() => _OrdersPageState();
-}
-
-class _OrdersPageState extends State<OrdersPage> {
-  bool _isLoading = true;
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    Provider.of<OrderList>(
-      context,
-      listen: false,
-    ).loadOrders().then((_) {
-      setState(() {
-        _isLoading = false;
-      });
-    });
-  }
-
-  Future<void> _refreshOrders(BuildContext context) {
-    return Provider.of<OrderList>(
-      context,
-      listen: false,
-    ).loadOrders();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final OrderList orders = Provider.of(context);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).primaryColor,
         title: const Text('Meus Pedidos'),
       ),
       drawer: const AppDrawer(),
-      body: RefreshIndicator(
-        onRefresh: () => _refreshOrders(context),
-        child: Padding(
-          padding: EdgeInsets.all(8),
-          child: _isLoading
-              ? const Center(child: CircularProgressIndicator())
-              : ListView.builder(
-                  itemCount: orders.itemsCount,
-                  itemBuilder: (context, index) =>
-                      OrderWidget(order: orders.items[index]),
-                ),
-        ),
+      body: FutureBuilder(
+        future: Provider.of<OrderList>(context, listen: false).loadOrders(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.error != null) {
+            return const Center(
+              child: Text('Ocorreu um erro!'),
+            );
+          } else {
+            return Consumer<OrderList>(
+              builder: (context, orders, child) => ListView.builder(
+                itemCount: orders.itemsCount,
+                itemBuilder: (context, index) =>
+                    OrderWidget(order: orders.items[index]),
+              ),
+            );
+          }
+        },
       ),
     );
   }
